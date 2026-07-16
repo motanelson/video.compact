fe147734e4243d9eba43402930918d89b95c7318
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-16 23:48:07 +0100

    Add files via upload

--- video.py
+++ video.py
@@ -1,5 +1,16 @@
 import os
 import copy
+import zlib
+def save_compile(f1, data):
+    comp = zlib.compress(data, level=9)
+    
+    f1.write(comp)
+
+def load_compile(filename):
+    with open(filename, "rb") as f:
+        comp = f.read()
+    return zlib.decompress(comp)
+
 print("\033c\033[47;30m\ngive me the .txt .video file ? \n")
 a=input().strip()
 f1=open(a,"r")
@@ -26,6 +37,6 @@ for d in ff:
         f1.write(b"\x01\x00\x05\x04\x03\x07")
         f1.write(d.strip().encode())
         f1.write(b"\x01\x00\x05\x04\x03\x02")
-        f1.write(fff)
+        save_compile(f1,fff)
         f1.close()
     counter=counter+1
